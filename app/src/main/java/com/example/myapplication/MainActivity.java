c3ab413ba401691891fbb58e20a0971ee94e530d
abdul <abdul.tanveer@gmail.com>
2019-07-09 04:59:27 +0530

    shared prefs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -13,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
     TextView mTextView;
+    EditText nameEditText,passwordEditText;
+    public static String FILE_NAME = "sageit";
+    public static String NAME_KEY = "name";
+    public static String PASSWORD_KEY = "pwd";
 
+
+    public  static  int MODE = Activity.MODE_PRIVATE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,47 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar1);
         mTextView = findViewById(R.id.textViewResult);
         Toast.makeText(this, "mainactivity is awake", Toast.LENGTH_SHORT).show();
-        sendSms();
+        nameEditText = findViewById(R.id.editTextName);
+        passwordEditText = findViewById(R.id.editText2);
+        //sendSms();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    private void saveData() {
+        String name = nameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        //create a file
+        SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        //open file
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //write to file
+        editor.putString(NAME_KEY, name);
+        editor.putString(PASSWORD_KEY,password);
+        //save file
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        restoreData();
+    }
+
+    private void restoreData(){
+        //open the file
+        SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        //read the file
+        String name = sharedPreferences.getString(NAME_KEY,"");
+        String pass = sharedPreferences.getString(PASSWORD_KEY,"");
+        //put the data in edittexts
+        nameEditText.setText(name);
+        passwordEditText.setText(pass);
     }
 
     public void handleClick(View view) {
